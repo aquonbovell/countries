bbc9b7311644b56e4749638b0668ad5bd40f1d30
Aquon Bovell <aquon_bovell@outlook.com>
2023-08-09 15:25:03 -0400

    config next js

--- components/Search.tsx
+++ components/Search.tsx
@@ -23,7 +23,7 @@ export default function Search() {
   };
 
   return (
-    <form className="w-full py-5 md:w-96" onChange={handleSearch}>
+    <form className="w-full py-5 md:w-96">
       <label htmlFor="input" className="sr-only">
         Search for Country
       </label>
